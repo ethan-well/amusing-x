688ebe1a14c5c8b3aa5f4f47b6bcc36859eb2a11
ItsWewin <wewin11235@163.com>
2021-04-05 18:50:24 +0800

    adjust redis lock to amusing superfactory

--- services/amusinguserserv/xredis/lock/lock.go
+++ services/amusinguserserv/xredis/lock/lock.go
@@ -3,14 +3,12 @@ package lock
 import (
 	"amusingx.fit/amusingx/services/amusinguserserv/xredis"
 	"context"
-	"errors"
-	"fmt"
-	"github.com/go-redis/redis/v8"
+	"github.com/ItsWewin/superfactory/db/redis/redislocker"
 )
 
 type Locker struct {
 	Key    string
-	Value  string
+	Value  interface{}
 	Expire int64
 }
 
@@ -23,48 +21,14 @@ func NewLocker(key, value string, expire int64) *Locker {
 }
 
 func (l *Locker) Lock(ctx context.Context) error {
-	script := redis.NewScript(`
-		if redis.call("SETNX", KEYS[1], ARGV[1]) == 1 then
-			return redis.call("expire", KEYS[1], ARGV[2])
-		else
-			return 0
-		end
-	`)
-
-	result, err := script.Run(ctx, xredis.Client, []string{l.Key}, l.Value, l.Expire).Result()
-	if err != nil {
-		return err
-	}
-
-	if result.(int64) != 1 {
-		return errors.New(fmt.Sprintf("lock failed, result: %d", result))
-	}
-
-	return nil
+	return redislocker.Lock(ctx, xredis.Client, l.Key, l.Value, l.Expire)
 }
 
 func (l *Locker) Unlock(ctx context.Context) error {
-	script := redis.NewScript(`return redis.call('del', KEYS[1]) ~= false`)
-
-	_, err := script.Run(ctx, xredis.Client, []string{l.Key}).Result()
-	if err != nil {
-		return err
-	}
-
-	return nil
+	return redislocker.Unlock(ctx, xredis.Client, l.Key)
 }
 
 // key 和 value 同时相等的时候，更新过期时间
 func (l *Locker) Refresh(ctx context.Context) error {
-	script := redis.NewScript(`if redis.call("get", KEYS[1]) == ARGV[1] then return redis.call("expire", KEYS[1], ARGV[2]) else return 0 end`)
-
-	r, err := script.Run(ctx, xredis.Client, []string{l.Key}, l.Value, l.Expire).Result()
-	if err != nil {
-		return err
-	}
-	if r.(int64) != 1 {
-		return errors.New(fmt.Sprintf("refresh failed, key: %s, value: %s, expire: %d", l.Key, l.Value, l.Expire))
-	}
-
-	return nil
+	return redislocker.Refresh(ctx, xredis.Client, l.Key, l.Value, l.Expire)
 }
