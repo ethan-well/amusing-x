5912dd87b762a5bcf365cdfa26229a556f5b3a79
ItsWewin <wewin11235@163.com>
2021-12-01 14:19:33 +0800

    request valid

---
+++ protos/ganymede/service/verification_code_valid.go
@@ -0,0 +1,42 @@
+package userservice
+
+import (
+	"amusingx.fit/amusingx/regexp"
+	"github.com/ItsWewin/superfactory/xerror"
+)
+
+const login = "login"
+const join = "join"
+
+var verificationCodeActions = []string{login, join}
+
+func (r *VerificationCodeRequest) IsLogin() bool {
+	return r.Action == login
+}
+
+func (r *VerificationCodeRequest) IsJoin() bool {
+	return r.Action == join
+}
+
+func (r *VerificationCodeRequest) Valid() *xerror.Error {
+	if err := regexp.PhoneNumberValid(r.Phone); err != nil {
+		return err
+	}
+
+	if err := regexp.AreaCodeValid(r.AreaCode); err != nil {
+		return err
+	}
+
+	actionValid := false
+	for _, action := range verificationCodeActions {
+		if r.Action == action {
+			actionValid = true
+			break
+		}
+	}
+	if !actionValid {
+		return xerror.NewError(nil, xerror.Code.CParamsError, "'action' s is invalid")
+	}
+
+	return nil
+}
