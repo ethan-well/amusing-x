dcaa6fbbceb6290b16e0092d31d89ddd307cb1f5
魏巍 <wewin11235@163.com>
2022-03-27 17:44:43 +0800

    suport sub product

--- services/charon/rpcserver/charonserver/sub_product.go
+++ services/charon/rpcserver/charonserver/sub_product.go
@@ -2,21 +2,26 @@ package charonserver
 
 import (
 	"amusingx.fit/amusingx/protos/pangu/service/pangu/proto"
+	"amusingx.fit/amusingx/services/charon/rpcserver/controller/subproduct"
 	"context"
 )
 
 func (s *CharonServer) SubProductCreate(ctx context.Context, in *proto.SubProductCreateRequest) (*proto.SubProduct, error) {
-	return nil, nil
+	return subproduct.HandlerCreate(ctx, in)
 }
+
 func (s *CharonServer) SubProductDelete(ctx context.Context, in *proto.SubProductDeleteRequest) (*proto.SubProductDeleteResponse, error) {
-	return nil, nil
+	return subproduct.HandlerDelete(ctx, in)
 }
+
 func (s *CharonServer) SubProducts(ctx context.Context, in *proto.SubProductListRequest) (*proto.SubProductListResponse, error) {
-	return nil, nil
+	return subproduct.HandlerList(ctx, in)
 }
+
 func (s *CharonServer) SubProduct(ctx context.Context, in *proto.SubProductRequest) (*proto.SubProduct, error) {
-	return nil, nil
+	return subproduct.HandlerQuery(ctx, in)
 }
+
 func (s *CharonServer) SubProductUpdate(ctx context.Context, in *proto.SubProductUpdateRequest) (*proto.SubProduct, error) {
-	return nil, nil
+	return subproduct.HandlerUpdate(ctx, in)
 }
