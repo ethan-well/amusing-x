28d5f6d0a8bc6039bed8f66fc8bfde046d2d5fea
魏巍 <wewin11235@163.com>
2022-03-27 11:29:33 +0800

    update sub_product sql schemas

--- services/charon/mysql/charon/schemas/create_table_sub_product_1639659189.sql
+++ services/charon/mysql/charon/schemas/create_table_sub_product_1639659189.sql
@@ -1,6 +1,8 @@
 CREATE TABLE `sub_product` (
                                `id` bigint NOT NULL AUTO_INCREMENT,
                                `product_id` bigint NOT NULL DEFAULT '0' COMMENT 'product id',
+                               `currency` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL DEFAULT '' COMMENT 'price currency',
+                               `price` int  NOT NULL DEFAULT '0' COMMENT 'price x 100',
                                `create_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
                                `update_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
                                PRIMARY KEY (`id`)
