fda1dfbeefcffa0ffba1e29e86d74b335723eed0
魏巍 <wewin11235@163.com>
2022-03-27 11:30:16 +0800

    update sub_product sql schemas

--- services/charon/mysql/charon/schemas/create_table_product_1639659152.sql
+++ services/charon/mysql/charon/schemas/create_table_product_1639659152.sql
@@ -1,9 +1,8 @@
-CREATE TABLE `product`
-(
-    `id`          bigint                                                 NOT NULL AUTO_INCREMENT,
-    `name`        varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL DEFAULT '0' COMMENT 'product name',
-    `desc`        varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL DEFAULT '0' COMMENT 'product desc',
-    `create_time` datetime                                               NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    `update_time` datetime                                               NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+CREATE TABLE `product` (
+                           `id` bigint NOT NULL AUTO_INCREMENT,
+                           `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL DEFAULT '0' COMMENT 'product name',
+                           `description` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL DEFAULT '0' COMMENT 'product description',
+                           `create_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+                           `update_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+                           PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=23 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
--- services/charon/mysql/charon/schemas/create_table_product_stock_1639659198.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE `product_stock` (
-                                 `id` bigint NOT NULL AUTO_INCREMENT,
-                                 `sub_product_id` bigint NOT NULL DEFAULT '0' COMMENT 'sub product id',
-                                 `value` bigint NOT NULL DEFAULT '0' COMMENT 'sub product stock',
-                                 `create_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-                                 `update_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-                                 PRIMARY KEY (`id`),
-                                 UNIQUE KEY `uk_sub_product_id` (`sub_product_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
